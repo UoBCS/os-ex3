ccc5e519ce051394f0175725d99cb1efc4a922af
oxe410 <oxe410@cs.bham.ac.uk>
2016-11-05 22:45:38 +0000

    Initial commit

--- charDeviceDriver.c
+++ charDeviceDriver.c
@@ -15,94 +15,188 @@
 
 MODULE_LICENSE("GPL");
 
+DEFINE_MUTEX(dev_lock);
+
 int lastWritten = 0;
 int lastRead = 0;
-/* 
- * This function is called whenever a process tries to do an ioctl on our
- * device file. We get two extra parameters (additional to the inode and file
- * structures, which all device functions get): the number of the ioctl called
- * and the parameter given to the ioctl function.
- *
- * If the ioctl is write or read/write (meaning output is returned to the
- * calling process), the ioctl call returns the output of this function.
- *
- */
 
-
-static long device_ioctl(struct file *file,	/* see include/linux/fs.h */
-		 unsigned int ioctl_num,	/* number and param for ioctl */
-		 unsigned long ioctl_param)
+// Linked list implementation
+// --------------------------
+int ls_append(struct linked_list *ls, char *buf)
 {
-    // TODO: Complete me
-    return 0;
+	struct node *cur = ls->head;
+	while (cur->next != NULL)
+		cur = cur->next;
+
+	cur->next = kmalloc(sizeof(struct node), GFP_KERNEL);
+	if (!cur->next)
+		return 0;
+	cur->next->msg = buf;
+	cur->next->next = NULL;
+
+	// Update total size
+	ls->total_sz += strlen(buf) * sizeof(char);
+
+	return 1;
 }
 
+int ls_remove(struct linked_list *ls, char **buf)
+{
+	struct node *next_node = NULL;
 
-/*
- * This function is called when the module is loaded
- */
+	if (ls->head == NULL)
+		return 0;
+
+	next_node = ls->head->next;
+
+	int len = strlen(ls->head->msg);
+	size_t msg_sz = len * sizeof(char);
+
+	*buf = kmalloc(msg_sz, GFP_KERNEL);
+	strcpy(*buf, ls->head->msg);
+
+	// Update total size
+	ls->total_sz -= msg_sz;
+
+	kfree(ls->head);
+	ls->head = next_node;
+
+	return 1;
+}
+
+int ls_destroy(struct linked_list *ls)
+{
+	struct node *cur = ls->head;
+	struct node* tmp;
+
+	while (cur != NULL) {
+		tmp = cur;
+		cur = cur->next;
+		kfree(tmp);
+	}
+
+	ls->head = NULL;
+	ls->total_sz = 0;
+
+	return 1;
+}
+
+// Character device driver implementation
+// --------------------------------------
+
+static long device_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param)
+{
+	// RESET thing?
+
+	int new_max = (int)ioctl_param;
+
+	if (new_max > max_msg_ls_len || new_max > msg_ls.total_sz) {
+		max_msg_ls_len = new_max;
+		return SUCCESS;
+	}
+
+	return -EINVAL;
+}
+
 int init_module(void)
 {
+	Major = register_chdev(0, DEVICE_NAME, fops);
 
-    Major = 0;
-    // TODO: Complete me
-    
-    // DO NOT CHANGE THESE LINES HERE, THEY ARE USED BY THE TEST SCRIPTS TO
-    // FIND THE RIGHT MAJOR/MINOR NUMBERS
-    printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
-    printk(KERN_INFO "the driver, create a dev file with\n");
-    printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+	if (Major < 0) {
+		printk(KERN_ALERT "Registering char device failed with %d\n", Major);
+		return Major;
+	}
 
-    return SUCCESS;
+	msg_ls.head = NULL;
+	msg_ls.total_sz = 0;
+
+	// DO NOT CHANGE THESE LINES HERE, THEY ARE USED BY THE TEST SCRIPTS TO
+	// FIND THE RIGHT MAJOR/MINOR NUMBERS
+	printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
+	printk(KERN_INFO "the driver, create a dev file with\n");
+	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+
+	return SUCCESS;
 }
 
-/*
- * This function is called when the module is unloaded
- */
 void cleanup_module(void)
 {
-    // TODO: Complete me
+	// Unregister the device
+	int ret = unregister_chrdev(Major, DEVICE_NAME);
+	if (ret < 0)
+		printk(KERN_ALERT "An error occurred when unregistering the device: %d\n", ret);
+
+	// Destroy message list
+	ls_destroy(&msg_ls);
 }
 
-/*
- * Methods
- */
-
-/* 
- * Called when a process tries to open the device file, like
- * "cat /dev/mycharfile"
- */
 static int device_open(struct inode *inode, struct file *file)
 {
-    // TODO: Complete me
-    return 0;
+	mutex_lock(&dev_lock);
+	if (Device_Open) {
+		mutex_unlock(&dev_lock);
+		return -EBUSY;
+	}
+	Device_Open++;
+	mutex_unlock(&dev_lock);
+
+	if (!ls_remove(&msg_ls, &msg_ptr))
+		return -EAGAIN;
+
+	try_module_get(THIS_MODULE);
+
+	return SUCCESS;
 }
 
-/* Called when a process closes the device file. */
 static int device_release(struct inode *inode, struct file *file)
 {
-    // TODO: Complete me
-    return 0;
+	mutex_lock(&dev_lock);
+	Device_Open--;
+	mutex_unlock(&dev_lock);
+	module_put(THIS_MODULE);
+
+	return SUCCESS;
 }
 
-/* 
- * Called when a process, which already opened the dev file, attempts to
- * read from it.
- */
-static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
-			   char __user *buffer,	/* buffer to fill with data */
-			   size_t length,	/* length of the buffer     */
-			   loff_t * offset)
+static ssize_t device_read(struct file *filp, char __user *buffer, size_t length, loff_t * offset)
 {
-    // TODO: Complete me
-    return 0;
+	int bytes_read = 0;
+
+	if (*msg_ptr == 0)
+		return 0;
+
+	char *ptr = msg_ptr;
+
+	while (length && *ptr) {
+		put_user(*(ptr++), buffer++);
+
+		length--;
+		bytes_read++;
+	}
+
+	free(msg_ptr);
+	msg_ptr = NULL;
+
+	return bytes_read;
 }
-    
-    
-/* Called when a process writes to dev file */
-static ssize_t
-device_write(struct file *filp, const char __user *buff, size_t len, loff_t * off)
+
+static ssize_t device_write(struct file *filp, const char __user *buffer, size_t length, loff_t * offset)
 {
-    // TODO: Complete me
-    return 0;
+	size_t msg_sz = length * sizeof(char);
+
+	if (msg_sz > max_msg_len) {
+		printk(KERN_ALERT "Error: the message size is bigger than 4K");
+		return -EINVAL;
+	}
+
+	if (msg_ls->total_sz + msg_sz > max_msg_ls_len) {
+		printk(KERN_ALERT "Error: the total messages size is bigger than the maximum allowed")
+		return -EAGAIN;
+	}
+
+	if (!ls_append(&msg_ls, buffer)) {
+		printk(KERN_ALERT "An error occurred when adding the message");
+	}
+
+	return length;
 }
--- charDeviceDriver.h
+++ charDeviceDriver.h
@@ -1,5 +1,47 @@
-/* Global definition for the example character device driver */
+#define SUCCESS 0
+#define DEVICE_NAME "opsysmem"	/* Dev name as it appears in /proc/devices   */
+// #define BUF_LEN 80		/* Max length of the message from the device */
 
+/* 
+ * Global variables are declared as static, so are global within the file. 
+ */
+//struct cdev *my_cdev;
+//dev_t dev_num;
+
+static int Major;		/* Major number assigned to our device driver */
+
+static struct file_operations fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
+};
+
+static size_t max_msg_len = 4 * 1024 * sizeof(char);
+static size_t max_msg_ls_len = 2 * (1024 * 1024) * sizeof(char);
+
+struct node {
+	char *msg;
+	struct node *next;
+};
+
+struct linked_list {
+	struct node *head;
+	size_t total_sz;
+};
+
+//static struct node *msg_ls;
+static struct linked_list msg_ls;
+static char *msg_ptr;
+
+// Linked list definition
+// ----------------------
+int ls_append(struct linked_list*, char*);
+int ls_remove(struct linked_list*, char**);
+int ls_destroy(struct linked_list*);
+
+// Device driver definition
+// ------------------------
 int init_module(void);
 void cleanup_module(void);
 static int device_open(struct inode *, struct file *);
@@ -7,18 +49,3 @@ static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 static long device_ioctl(struct file *file, unsigned int ioctl_num, unsigned long);
-
-#define SUCCESS 0
-#define DEVICE_NAME "opsysmem"	/* Dev name as it appears in /proc/devices   */
-#define BUF_LEN 80		/* Max length of the message from the device */
-
-/* 
- * Global variables are declared as static, so are global within the file. 
- */
-struct cdev *my_cdev;
-       dev_t dev_num;
-
-static int Major;		/* Major number assigned to our device driver */
-
-
- // TODO: Complete me
